833ad3221f137854ed98b7e7ed79f22fef345416
Kashish Garg <kashishgarg9@gmail.com>
2024-08-25 19:01:52 -0400

    added unions vs structs to learning

--- c_learning/src/keywords.c
+++ c_learning/src/keywords.c
@@ -30,8 +30,27 @@ void global_fn_example();
  * Note they do have to linked together during compilation
 */
 extern int extern_example;
-extern void print_extern_example();
+extern void print_extern_example(int a);
 
+/*
+ * Unions vs Structs. The struct will allocate 12 bytes of memory and the union will 
+ * allocate only 4. Essentially the struct assumes you will fill out all of the members
+ * within it, while the union only assumes you will fill out only one of the members.
+ * It is recommended to use unions within structs for specific fields in case you only
+ * need one of the n number of those fields.
+*/
+
+typedef struct {
+  int x; // 4 bytes
+  float y; // 4 bytes
+  char z; // 1 byte
+} struct_example; // total 12 bytes, due to struct packing
+
+typedef union {
+  int x; // 4 bytes
+  float y; // 4 bytes
+  char z; // 1 byte
+} union_example; // total 4 bytes
 
 int main() {
   // Static Variable Example
@@ -40,11 +59,17 @@ int main() {
   static_example(a);
 
   // Static Function Example
-  // static_fn_example(); --> Compiler Error: function is static and therefore file scoped only
+  // static_fn_example(); --> Compiler Error: function is static and therefore file scoped
+  // only
   global_fn_example();
 
   // Extern function and variable example
   print_extern_example(a);
+
+  printf("Size of Struct: %lu\n", sizeof(struct_example)); // 12
+  printf("Size of Union: %lu\n", sizeof(union_example)); // 4
   
+
+
   return 1;
 }
