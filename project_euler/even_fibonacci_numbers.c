87efd171c06488a10b5ce108deb897740804a8c1
Kashish Garg <kashishgarg9@gmail.com>
2024-08-26 18:53:07 -0400

    even fibonacci numbers pe problem

--- project_euler/even_fibonacci_numbers.c
+++ project_euler/even_fibonacci_numbers.c
@@ -1,8 +1,31 @@
 #include <stdio.h>
+#define THRESHOLD 4000000
 
 int main() {
 
-  int val_1, val_2;
+  int val_1, val_2, even_sum_counter;
+  val_1 = 1;
+  val_2 = 2;
+
+  even_sum_counter = 2;
+  
+  while (val_1 < THRESHOLD && val_2 < THRESHOLD) {
+    val_1 = val_1 + val_2;
+    val_2 = val_1 + val_2;
+
+    if (val_1 % 2 == 0) {
+      even_sum_counter += val_1;
+    }
+
+    if (val_2 % 2 == 0) {
+      even_sum_counter += val_2;
+    }
+  }
+
+  printf("%d\n", even_sum_counter);
+
+
+  
 
   return 0;
 }
