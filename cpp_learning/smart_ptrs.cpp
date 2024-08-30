17d8c501646127b0d715df9b5f0d33303419c013
kash <kashishgarg9@gmail.com>
2024-08-30 13:17:35 -0400

    Added smart pointers

---
+++ cpp_learning/smart_ptrs.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+// This file is so that we can play around with smart pointers, beginning with Shared and Unique Ptrs, specifically
+//
+// A std::shared_ptr is a pointer that allows multiple pointers to have ownership over a resource, once all of the 
+// pointers have been deleted, the object itself will be deleted
+//
+// A std::unique_ptr is a pointer that allows one AND ONLY one pointer to have ownership over a resource. This 
+// resource can be moved using std::move. Once the single owner has been deleted, that's when the object dies
+int main() {
+
+
+  return 0;
+}
