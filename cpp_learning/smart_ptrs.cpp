aebd82e62ec220b1027f222c37f8133eabae0833
Kashish Garg <kashishgarg9@gmail.com>
2024-09-01 12:30:01 -0400

    Save point before getting into HAL_I2C

--- cpp_learning/smart_ptrs.cpp
+++ cpp_learning/smart_ptrs.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <memory>
 
 // This file is so that we can play around with smart pointers, beginning with Shared and Unique Ptrs, specifically
 //
@@ -7,6 +8,9 @@
 //
 // A std::unique_ptr is a pointer that allows one AND ONLY one pointer to have ownership over a resource. This 
 // resource can be moved using std::move. Once the single owner has been deleted, that's when the object dies
+
+// The idea of ownership is "who is responsible for destroying the object at the end of its use/lifetime"
+// Unique pointers 
 int main() {
 
 
