adffa7a72aba62ef328773b62773d0c1ee5c5adf
Kashish Garg <kashishgarg9@gmail.com>
2025-01-05 23:18:14 -0500

    selectionsort in c

--- crushing_embedded/c/sorting.c
+++ crushing_embedded/c/sorting.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <limits.h>
 
 // File for implementation on different sorting algorithms all on C Arrays
 int* insertionSort(int* outputArray, int size) {
@@ -30,6 +31,25 @@ int* insertionSort(int* outputArray, int size) {
   return outputArray;
 }
 
+void selectionSort(int* inputArray, int size) {
+  // takes the most minimum value and appends to the front
+  for (int i = 0; i < size; i++) {
+    int min_val = INT_MAX;
+    int j_ind = 0;
+    for (int j= i; j < size; j++) {
+      if (inputArray[j] < min_val) {
+        min_val = inputArray[j];
+        j_ind = j;
+      }
+    }
+    // min_val now contains the most minimum value in the sequence
+    // swap these two indices
+    inputArray[j_ind] = inputArray[i];
+    inputArray[i] = min_val;
+    
+  }
+}
+
 int main() {
 
   int arr[] = {
@@ -37,6 +57,7 @@ int main() {
     745827, 302849, 584920, 173849, 290384, 948203, 284730, 561839, 723890, 184927,
     284637, 902384, 467281, 104738, 283745, 876924, 129083, 294827, 764930, 847392
   };
-  insertionSort(arr, 30);
+  // insertionSort(arr, 30);
+  selectionSort(arr, 30);
   return 0;
 }
